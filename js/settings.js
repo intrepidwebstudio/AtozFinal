53084b2bd0b06923e7b719791b17d20325a1d963
intrepidwebstudio <sunil@inpws.com>
2015-04-16 14:39:07 +0530

    qqq

--- js/settings.js
+++ js/settings.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 var ajaxpath="http://192.168.1.6/atoz/app/";
-
+//var ajaxpath="http://www.safelearners.com/atoz/app/";
 var	globalusername='';
 var	globaluserid='';
 var	globaluserphone='';
