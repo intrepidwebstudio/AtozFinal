cb9a447200a537b4252928a63cb46d16389e674d
intrepidwebstudio <sunil@inpws.com>
2015-04-16 22:23:49 +0530

    1234567

--- js/code.js
+++ js/code.js
@@ -1,3 +1,31 @@
+		var $ = jQuery.noConflict();
+		$(function(){
+		$('.form').find('input, select, textarea').on('touchstart mousedown click', function(e){
+		e.stopPropagation();
+		})
+		})
+		
+		var swiperParent = new Swiper('.swiper-parent',{
+		//pagination: '.pagination',
+		mode: 'horizontal',
+		noSwiping: true,
+		paginationClickable: true,
+		allowSwipeToPrev:false,
+		allowSwipeToNext:false,
+		onSlideChangeEnd : function() {
+				//Do something when you touch the slide
+				/*		if (swiperParent.activeIndex != 0){
+				$('#header').animate({'top':'0px'},400);
+				}
+				if (swiperParent.activeIndex == 0){
+				$('#header').animate({'top':'-100px'},400);
+				}  
+				*/
+		  }
+		})
+
+
+
 
 $('.swiper-nested').each(function(){
 	var swipernested = $(this).swiper({
@@ -153,37 +181,3 @@ $(".post_more").click(function(){
 
 
 
-    // This button will increment the value
-    $('.qtyplus').click(function(e){
-        // Stop acting like a button
-        e.preventDefault();
-        // Get the field name
-        fieldName = $(this).attr('field');
-        // Get its current value
-        var currentVal = parseInt($('input[name='+fieldName+']').val());
-        // If is not undefined
-        if (!isNaN(currentVal)) {
-            // Increment
-            $('input[name='+fieldName+']').val(currentVal + 1);
-        } else {
-            // Otherwise put a 0 there
-            $('input[name='+fieldName+']').val(1);
-        }
-    });
-    // This button will decrement the value till 0
-    $(".qtyminus").click(function(e) {
-        // Stop acting like a button
-        e.preventDefault();
-        // Get the field name
-        fieldName = $(this).attr('field');
-        // Get its current value
-        var currentVal = parseInt($('input[name='+fieldName+']').val());
-        // If it isn't undefined or its greater than 0
-        if (!isNaN(currentVal) && currentVal > 0) {
-            // Decrement one
-            $('input[name='+fieldName+']').val(currentVal - 1);
-        } else {
-            // Otherwise put a 0 there
-            $('input[name='+fieldName+']').val(1);
-        }
-    });
