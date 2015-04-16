ada49b876c2186f3a80c40db10a05d8ca8fae31d
intrepidwebstudio <sunil@inpws.com>
2015-04-16 22:02:19 +0530

    122

--- js/load.js
+++ js/load.js
@@ -29,15 +29,89 @@ $(document).ready(function () {
 		allowSwipeToNext:false,
 		onSlideChangeEnd : function() {
 		//Do something when you touch the slide
-		if (swiperParent.activeIndex != 0){
+/*		if (swiperParent.activeIndex != 0){
 		$('#header').animate({'top':'0px'},400);
 		}
 		if (swiperParent.activeIndex == 0){
 		$('#header').animate({'top':'-100px'},400);
 		}  
+		*/
+		
 		}
 		})
 
 
 	
 });
+
+
+$('.swiper-nested').each(function(){
+	var swipernested = $(this).swiper({
+		mode:'vertical',
+		scrollContainer: true,
+		mousewheelControl: true,
+		scrollbar: {
+			container:$(this).find('.swiper-scrollbar')[0]
+		}
+	})
+	$(".scrolltop").click(function() {					  
+		swipernested.swipeTo(0);
+	})
+	$(".trigger").click(function(){
+		function fixheighttrigger()
+		{
+			swipernested.reInit();
+			setTimeout(fixheighttrigger, 1000);
+		}
+		setTimeout(fixheighttrigger, 1000);	
+	});
+	$(".trigger_blog").click(function(){
+		function fixheighttoogle()
+		{
+			swipernested.reInit();
+			setTimeout(fixheighttoogle, 1000);
+		}
+		setTimeout(fixheighttoogle, 1000);
+	});
+	$(".tabsmenu li").click(function(){
+		function fixheight()
+		{
+			swipernested.reInit();
+			setTimeout(fixheight, 1000);
+		}
+        setTimeout(fixheight, 1000);
+	});
+	$("#loadMore").click(function(){
+		function fixheightposts()
+		{
+			swipernested.reInit();
+			setTimeout(fixheightposts, 1000);
+		}
+        setTimeout(fixheightposts, 1000);
+	});
+	
+	$(".post_details_page1 li").hide();
+	$(".posts1 li div span.detail").click(function(){
+	
+		p_ID = this.id;
+		
+		$(".post_details_page1").find("li").each(function() { 
+			if(this.id == p_ID)
+			{
+				$(".posts_archive_page1").hide(); 
+				var detailspostid = $(".post_details_page1 li#" + this.id);
+				detailspostid.show();
+				swipernested.reInit();
+				swipernested.swipeTo(0);
+				$('.backtoblog').click(function(){
+					 detailspostid.hide();
+					 $(".posts_archive_page1").show();
+					 swipernested.reInit();
+				});	
+			}
+		});
+	
+	});
+
+	
+})
