251fac0d597ec962ee74cba6db958132a8fb01df
intrepidwebstudio <sunil@inpws.com>
2015-04-16 21:34:57 +0530

    1234

--- js/code.js
+++ js/code.js
@@ -132,3 +132,17 @@ $(".post_more").click(function(){
 });
 
 
+
+	$(".posts1 li").hide();	
+    size_li = $(".posts1 li").size();
+    x=9;
+    $('.posts1 li:lt('+x+')').show();
+	
+    $('#loadMore').click(function () {
+        x= (x+1 <= size_li) ? x+1 : size_li;
+        $('.posts1 li:lt('+x+')').show();
+        if(x == size_li){
+            $('#loadMore').hide();
+			$('#showLess').show();
+        }
+    });
