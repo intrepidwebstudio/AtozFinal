0cc99924cf1192fb0ca004ad920490cd83c94d6e
intrepidwebstudio <sunil@inpws.com>
2015-04-16 22:37:04 +0530

    12
    2

--- js/settings.js
+++ js/settings.js
@@ -1,7 +1,9 @@
 // JavaScript Document
-var ajaxpath="http://192.168.1.6/atoz/app/";
-var imagepath="http://192.168.1.6/atoz/";
-//var ajaxpath="http://www.safelearners.com/atoz/app/";
+//var ajaxpath="http://192.168.1.6/atoz/app/";
+//var imagepath="http://192.168.1.6/atoz/";
+
+var ajaxpath="http://www.safelearners.com/atoz/app/";
+var imagepath="http://www.safelearners.com/atoz/";
 var	globalusername='';
 var	globaluserid='';
 var	globaluserphone='';
